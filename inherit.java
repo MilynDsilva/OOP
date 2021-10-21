3ed09c01255e5d0468eb72b1a4ba280baaed6daa
Milyn <milyng.dsilva@gmail.com>
2021-10-22 00:51:24 +0530

    Added an inherit file

---
+++ inherit.java
@@ -0,0 +1,13 @@
+class fruits {
+    String taste = "Sweet";
+    String nature = "Dry";
+}
+public class inherit extends fruits {
+    String color ="Yellow";
+
+    public static void main(String args[]) {
+       inherit object1 = new inherit();
+       System.out.println(object1.nature+" fruits are "+object1.taste+" and Mangoo is "+object1.color+" in color");
+
+    }
+}
