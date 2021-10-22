c845f2d9c839d556208ce3b13b76a71347517d61
Milyn <milyng.dsilva@gmail.com>
2021-10-22 14:34:28 +0530

    added new file passbyref

---
+++ passbyref.java
@@ -0,0 +1,34 @@
+class passbyref{  
+
+        
+    public static void main(String args[]){  
+        
+        int s1=10;
+
+        modify(s1);
+        System.out.println(s1);
+    }
+    public static void modify(int s2) {
+        s2=33;
+        System.out.println(s2);
+    }  
+   }  
+   class square1 {
+       float l;
+       float b;
+       
+           public float getl() {
+                return l;
+           }
+           public void setl(float l) {
+                this.l=l;
+           }
+           public float getb() {
+            return b;
+        }
+        public void setb(float b) {
+            this.b=b;
+        
+       }
+
+    }
---
+++ passbyval.java
@@ -0,0 +1,55 @@
+class passbyval{  
+
+        
+    public static void main(String args[]){  
+        square s1= new square(); //ref n costruct
+        s1.l=10;
+
+        modify(s1);
+        System.out.println(s1.l);
+    }
+    public static void modify(square s2) {
+        s2.l=33;
+        System.out.println(s2.l);
+    }  
+   }  
+   class square {
+       float l;
+       float b;
+       
+           public float getl() {
+                return l;
+           }
+           public void setl(float l) {
+                this.l=l;
+           }
+           public float getb() {
+            return b;
+        }
+        public void setb(float b) {
+            this.b=b;
+        
+       }
+
+    }
+
+
+
+
+// class passbyval{  
+//     int data=50;  
+     
+//     void change(int data){  
+//     data=data+100;//changes will be in the local variable only  
+//     System.out.println(data);
+//     }  
+        
+//     public static void main(String args[]){  
+//         passbyval op=new passbyval();  //instantiate
+     
+//       System.out.println("before change "+op.data);  
+//       op.change(500);  
+//       System.out.println("after change "+op.data);  
+     
+//     }  
+//    }  
