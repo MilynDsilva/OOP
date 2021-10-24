a9d68c73b074844fce9309d8d686e5e2ad0231f1
Milyn <milyng.dsilva@gmail.com>
2021-10-24 22:27:47 +0530

    Tested overloading with/without constructors

---
+++ TestOverloading.java
@@ -0,0 +1,24 @@
+class multiwithconstructor {
+    static int multiwithconstructor(int a,int b){
+        return a*b;
+    }
+    static int multiwithconstructor(int a,int b,int c){
+        return a*b*c;
+    }
+}
+class multiwithoutconstructor {
+    static int multi(int a,int b){
+        return a*b;
+    }
+    static int multi(int a,int b,int c){
+        return a*b*c;
+    }
+}
+class TestOverloading{
+public static void main(String args[]) {
+    System.out.println(multiwithconstructor.multiwithconstructor(10,10));   
+    System.out.println(multiwithconstructor.multiwithconstructor(12,12,10));
+    System.out.println(multiwithoutconstructor.multi(11,10));   
+    System.out.println(multiwithoutconstructor.multi(10,11,10));
+}
+}
